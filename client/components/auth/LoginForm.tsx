e88b95685e68f2ac121590c00cc58e98c76b3dc1
MatviyO <oleh.m97@gmail.com>
2024-08-13 21:58:43 +0300

    feat: login form

---
+++ client/components/auth/LoginForm.tsx
@@ -0,0 +1,10 @@
+import {FC} from "react";
+import styles from "./LoginForm.module.scss"
+
+export const LoginForm: FC = () => {
+    return (
+        <div className={styles.formBlock}>
+            <h1>Login Form</h1>
+        </div>
+    )
+}
