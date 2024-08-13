1f1694ae641ee6143335378197d96d7044b2d4d6
MatviyO <oleh.m97@gmail.com>
2024-08-13 21:54:42 +0300

    feat: client

---
+++ client/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Page() {
+    return <>
+     <Head>
+        <title>Dashboard / Auth</title>
+     </Head>
+        <main style={{ width: '400px', margin: '50px auto' }}>
+
+        </main>
+    </>
+}
