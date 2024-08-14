38b61464dedf648b27661b47e499fb0657d74cf4
MatviyO <oleh.m97@gmail.com>
2024-08-15 00:33:29 +0300

    feat: login form

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,11 +1,22 @@
 import Head from "next/head";
+import {Tabs} from "antd";
+import {LoginForm} from "@/components/auth/LoginForm";
+
+const TABS = [
+    { key: "1", label: 'Login', children: <LoginForm />},
+    { key: "2", label: 'Registration', children: <h1>Registration</h1>},
+]
 
 export default function Page() {
     return <>
-     <Head>
-        <title>Dashboard / Auth</title>
-     </Head>
+         <Head>
+            <title>Dashboard / Auth</title>
+         </Head>
         <main style={{ width: '400px', margin: '50px auto' }}>
+            <Tabs
+                items={TABS}>
+
+            </Tabs>
 
         </main>
     </>
--- client/components/auth/LoginForm.tsx
+++ client/components/auth/LoginForm.tsx
@@ -1,10 +1,57 @@
 import {FC} from "react";
 import styles from "./LoginForm.module.scss"
+import {Button, Form, Input} from "antd";
 
 export const LoginForm: FC = () => {
+    const onSubmit = async (values: any) => {
+        console.log(values)
+    }
     return (
         <div className={styles.formBlock}>
-            <h1>Login Form</h1>
+            <Form
+                name="basic"
+                labelCol={{
+                    span: 8,
+                }}
+                onFinish={onSubmit}
+            >
+                <Form.Item
+                    label="E-Mail"
+                    name="email"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Enter email",
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Пароль"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Enter password",
+                        },
+                    ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                    wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                    }}
+                >
+                    <Button type="primary" htmlType="submit">
+                        Login
+                    </Button>
+                </Form.Item>
+            </Form>
         </div>
     )
 }
