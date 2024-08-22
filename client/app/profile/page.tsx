d788528a705c5de3640728a260dd508d2b2aef90
MatviyO <oleh.m97@gmail.com>
2024-08-22 20:35:22 +0300

    feat: profile

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -1,49 +1,32 @@
-import { NextPage } from "next";
-import { Button } from "antd";
-
 import styles from "@/styles/Profile.module.scss";
-import * as Api from "@/api";
-import React from "react";
-import {IUser} from "@/api/dto/IUser";
-import {checkUser} from "@/utils/ChekUser";
+import {getDashboardProfileServer} from "@/app/profile/getDashboardProfileServer";
+import {Layout} from "@/layouts/Layout";
+import LogoutButton from "@/app/profile/LogoutButton";
 
-interface Props {
-    userData: IUser;
-}
-
-const DashboardProfilePage: NextPage<Props> = async () => {
-    const userData = await checkUser();
-    if (!userData) {
-        location.href = "/login";
-    }
-
-    const onClickLogout = () => {
-        if (window.confirm("Are you sure?")) {
-            Api.auth.logout();
-            location.href = "/";
-        }
-    };
+const DashboardProfilePage = async () => {
+    const { userData } = await getDashboardProfileServer();
 
     return (
-        <main>
+        <Layout title="Profile">
             <div className={styles.root}>
                 <h1>Profile</h1>
                 <br/>
                 <p>
-                    ID: <b>{"id" in userData ? userData?.id : "-"}</b>
+                    ID: <b>{userData?.id || "-"}</b>
                 </p>
                 <p>
-                    Full Name: <b>{"fullName" in userData ? userData?.fullName : "-"}</b>
+                    Full Name: <b>{userData?.fullName || "-"}</b>
                 </p>
                 <p>
-                    E-Mail: <b>{"email" in userData ? userData?.email : "-"}</b>
+                    E-Mail: <b>{userData?.email || "-"}</b>
                 </p>
                 <br/>
-                <Button onClick={onClickLogout} type="primary" danger>
-                    Logout
-                </Button>
+                <LogoutButton />
             </div>
-        </main>
+        </Layout>
+
     );
 };
+
+
 export default DashboardProfilePage;
